1d7707ecf6b3bae2f261e3c1f4b085fcc74ca5d4
DaniK178 <daniellekyere@gmail.com>
2022-09-28 19:32:48 +0100

    created js and html files

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.log("Hi!");``
+//Fetch all countries from the above url, and save the results in a variable
+// Create a ul element in your index.html
+// Generate li elements for each country object and add the name of each country to the element's textContent value.
+// Append the li elements to the ul element to display a list of country names
+// Extensions
+// Add the flag next to the country's name, and resize it so it doesn't look ugly (use a .css file for this!)
+// Create at the top of your index.html a new h2 tag
+// Calculate the total population of all the countries, and display it in the h2 tag
