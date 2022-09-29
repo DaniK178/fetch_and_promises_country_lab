a685def9abfa1405f659cc84f630d405f27c99b4
DaniK178 <daniellekyere@gmail.com>
2022-09-29 09:31:06 +0100

    attempted extensions

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //console.log("Hi!");``
-//Fetch all countries from the above url, and save the results in a variable - yes
-// Create a ul element in your index.html -yes 
-// Generate li elements for each country object and add the name of each country to the element's textContent value.
-// Append the li elements to the ul element to display a list of country names
-//https://restcountries.com/v3.1/all
-//https://restcountries.com/v3.1/name/peru
 
 window.addEventListener("DOMContentLoaded", () => {
 
+    // async () => {
+    //     const response = await fetch("https://restcountries.com/v3.1/all");
+    //     const jsonData = await response.json();
+    //     console.log(jsonData)
+    // }  
+    // console.log(jsonData);
 
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
   .then((allCountries) => {
-
 
     console.log(allCountries); 
     
@@ -20,34 +19,37 @@ fetch("https://restcountries.com/v3.1/all")
     console.log(countryNames);
 
     countryNames.forEach((countryName) => {
-    const countryNameList = document.createElement ("li");
-    countryNameList.textContent = countryName;
-    console.log(countryNameList);
+        const countryNameList = document.createElement ("li");
+        countryNameList.textContent = countryName;
+        //console.log(countryNameList);
 
-    const CountryList = document.querySelector("ul");
-    CountryList.appendChild(countryNameList);
-        
+        const CountryList = document.querySelector("ul");
+        CountryList.appendChild(countryNameList);    
     });
-    
+
+   
 
 });
 
 })
 
 
+ 
 
 
-
-
-
-
-
-
-
-
-
- // Extensions
+// Extensions
 // Add the flag next to the country's name, and resize it so it doesn't look ugly (use a .css file for this!)
 // Create at the top of your index.html a new h2 tag
 // Calculate the total population of all the countries, and display it in the h2 tag
 
+//     const totalPopulation= () => {allCountries
+//         // const allCountryPopulation= allCountries.map((country)=>country.population)
+//         .map((country)=>country.population)
+//         //console.log(allCountriesPopulation())
+//         .reduce((agg, current) => agg + current, 0);
+//     }
+   
+//   //document.querySelector("h2").innerHTML = `Country Population ${totalPopulation()}`
+
+ // const countryFlags= allCountries.map((country)=>country.flag);
+    // console.log(countryFlags);
